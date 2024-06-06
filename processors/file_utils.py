aa33fe3b2ec166bc58e6d6ae38139411eb78c2f2
developer-sidani <developer.sidani@gmail.com>
2024-06-06 04:07:41 +0200

    fix: files to be executed

--- processors/file_utils.py
+++ processors/file_utils.py
@@ -273,7 +273,7 @@ def s3_get(url, temp_file, proxies=None):
 
 
 def http_get(url, temp_file, proxies=None, resume_size=0, user_agent=None):
-    ua = "transformers/{}; python/{}".format(__version__, sys.version.split()[0])
+    ua = "transformers/{}; python/{}".format(transformers.__version__, sys.version.split()[0])
     if isinstance(user_agent, dict):
         ua += "; " + "; ".join(
             "{}/{}".format(k, v) for k, v in user_agent.items()
